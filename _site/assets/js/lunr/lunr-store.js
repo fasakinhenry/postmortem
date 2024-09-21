9958ee687d44f8d90e45cbdd94b01da32083f444
Fasakin Henry <fasakinhenry@gmail.com>
2024-09-21 21:20:21 +0100

    fix: remove _site folder and gemfile.lock

--- _site/assets/js/lunr/lunr-store.js
+++
@@ -1,7 +0,0 @@
-var store = [{
-        "title": "Incident Postmortem - API Outage",
-        "excerpt":"Incident Summary On September 19, 2024, we experienced a 2-hour outage due to a misconfiguration in our database. Lead-Up Describe the events leading up to the incident. Fault Detail the technical issues or errors that caused the incident. Impact Explain the impact on users, services, etc. Detection How was the...","categories": ["example","sample"],
-        "tags": ["test","sample"],
-        "url": "/example/sample/2024/09/20/consolve_postmortem/",
-        "teaser": null
-      }]
